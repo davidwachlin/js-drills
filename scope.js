5c6e2e7348f74196362a58a227af2a2c703242df
David Wachlin <davidwachlin@gmail.com>
2021-03-09 11:31:19 -0500

    add scope

---
+++ scope.js
@@ -0,0 +1,9 @@
+// Create a variable called globalScopedVariable. Set it equal to "I am globally scoped."
+
+// Create function called myFunction.
+// In the function body, create a variable called functionScopedVariable. Set it equal to "I am inside a function"
+// On the second line of the function body, log functionScopedVariable.
+
+// Log globalScopedVariable.
+
+// Log functionScopedVariable.
